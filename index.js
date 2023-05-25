1d95ac630e6b48cee679669c037c9ec2b4c0aac4
gitesh <124420820+gitesh-singla@users.noreply.github.com>
2023-05-25 19:36:36 +0530

    basic functionality completed

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
 const gameboard = (() => {
-    const gameArray = ["X", "O", "X", "O", "X", "O", "X", "O", "O"];
+    const gameArray = ["", "", "", "", "", "", "", "", ""];
+    let turn = "X";
+    const board = document.querySelector('.board');
 
     const drawBoard = () => {
-        const board = document.querySelector('.board');
+        board.addEventListener('click', move);
+        let index = 0;
 
         gameArray.forEach(element => {
             const tile = document.createElement('div');
             tile.classList.add('tile');
+            tile.classList.add('no-select');
             tile.innerHTML = element;
+            tile.setAttribute("data-index", index);
+            index++;
             board.appendChild(tile);
         });
     }
 
+    const move = (event) => {
+        // console.log(event.target)
+        const tile = event.target;
+        if(tile.innerHTML != "") return;
+
+        tile.innerHTML = turn;
+        const index = tile.getAttribute('data-index');
+        gameArray[index] = turn;
+        if(checkGameOver()){
+            console.log(`${turn} : wins!`);
+            clearBoard();
+        }
+        turn = turn == "X"?"O":"X";
+    }
+
+    const checkGameOver = ()=> {
+        if((gameArray[0] == turn) && (gameArray[1] == turn) && (gameArray[2] == turn)){
+            return true;
+        }
+        if((gameArray[3] == turn) && (gameArray[4] == turn) && (gameArray[5] == turn)){
+            return true;
+        }
+        if((gameArray[6] == turn) && (gameArray[7] == turn) && (gameArray[8] == turn)){
+            return true;
+        }
+        if((gameArray[0] == turn) && (gameArray[3] == turn) && (gameArray[6] == turn)){
+            return true;
+        }
+        if((gameArray[1] == turn) && (gameArray[4] == turn) && (gameArray[7] == turn)){
+            return true;
+        }
+        if((gameArray[2] == turn) && (gameArray[5] == turn) && (gameArray[8] == turn)){
+            return true;
+        }
+        if((gameArray[0] == turn) && (gameArray[4] == turn) && (gameArray[8] == turn)){
+            return true;
+        }
+        if((gameArray[2] == turn) && (gameArray[4] == turn) && (gameArray[6] == turn)){
+            return true;
+        }
+        return false;
+    }
+
+    const clearBoard = () => {
+        gameArray.forEach((element, index) => {
+            gameArray[index] = "";
+        })
+        board.innerHTML = "";
+        turn = "X";
+        drawBoard();
+    }
+
     return {
-        drawBoard
+        drawBoard,
+        clearBoard,
     }
 })()
+
+function player (symbol){
+    const play = () => {
+
+    }
+}
 
 gameboard.drawBoard();
